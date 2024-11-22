006088464dfbf667565082f167cd2bb1f3467c0d
simplyObare <josephatowiny@gmail.com>
2024-11-22 16:20:27 +0300

    Automatically Create User Profiles

--- musker/social/musker/models.py
+++ musker/social/musker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create user profile model.
@@ -11,3 +12,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# Create profile when new user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
