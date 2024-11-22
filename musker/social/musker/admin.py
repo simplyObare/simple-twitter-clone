d90323463001d3d65c5f553716393e0ae13e9630
simplyObare <josephatowiny@gmail.com>
2024-11-22 15:47:44 +0300

    creating superuser, userAdmin and user profile

--- musker/social/musker/admin.py
+++ musker/social/musker/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+# unregister Groups.
+admin.site.unregister(Group)
+
+
+# mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extent User model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # just display username fields on the admin page
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+# unregister initial User
+admin.site.unregister(User)
+
+# Register your models and Profile here.
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
