0b87913dce33daba03574043e7382461a2eb722a
simplyObare <josephatowiny@gmail.com>
2024-11-22 18:06:13 +0300

    List Profiles Page For Our Twitter App

--- musker/social/musker/models.py
+++ musker/social/musker/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     follows = models.ManyToManyField(
         "self", related_name="followed_by", symmetrical=False, blank=True
     )
+    date_modified = models.DateTimeField(User, auto_now=True)
 
     def __str__(self):
         return self.user.username
--- musker/social/musker/views.py
+++ musker/social/musker/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 # Create your views here.
 def home(request):
+
     return render(request, "home.html", {})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, "profile_list.html", {"profiles": profiles})
+    else:
+        return render(request, "home.html", {})
